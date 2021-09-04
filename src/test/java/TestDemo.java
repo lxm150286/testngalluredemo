7c7b467bfcb7c41dcc5c38da0de9f0ed82541330
“lxm” <1369531009@qq.com>
2021-09-04 22:33:40 +0800

    first-commit

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @Test
+    public void f(){
+        assert 2==2;
+    }
+    @Test
+    public void f1(){
+        assert "45"=="";
+    }
+    @Test
+    public void f2(){
+        assert  "3533"=="3533";
+    }
+}
